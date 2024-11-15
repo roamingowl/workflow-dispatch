0b092d082f79ebf83c9a144ec14a3ba258cbbb07
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-11-15 13:28:22 +0100

    chore(action): wip

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,5 +1,12 @@
+import { describe, it, expect } from 'vitest';
+import { run } from '../main';
+
 describe('Main', () => {
   it('should work', async () => {
+    expect(true).toBe(true);
+  });
+  it('handleLogs', async () => {
+    await run();
     expect(true).toBe(true);
   });
 });
