3d8a756329f865c90eae582ed060a70a00f7563f
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-11-22 11:40:36 +0100

    chore(action): use ms for parsing miliseconds

--- src/__tests__/summary.test.ts
+++ src/__tests__/summary.test.ts
@@ -1,13 +1,27 @@
 import { describe, it, expect, vi } from 'vitest';
 import { context } from '@actions/github';
+import * as core from '@actions/core';
 import { printSummary } from '../summary';
 import { DEFAULT_JOB_SUMMARY_TEMPLATE } from '../templates/summary';
 
 vi.spyOn(context, 'repo', 'get').mockReturnValue({ owner: 'me', repo: 'repo' });
 
+vi.mock('@actions/core', async (importOriginal) => {
+  return {
+    ...(await importOriginal<typeof import('@actions/core')>()),
+    summary: { addRaw: vi.fn(() => {
+      return { write: vi.fn() }}) },
+  };
+});
+
 describe('printSummary', () => {
   it('should print summary', async () => {
-    const string = await printSummary('dummy.yaml', 'http://github.com/something', 'me/repo', true, true, DEFAULT_JOB_SUMMARY_TEMPLATE);
-    expect(string).toBe('Workflow dummy.yaml has been triggered.  \n');
+    const setOutputMock = vi.spyOn(core, 'setOutput').mockImplementation(vi.fn());
+
+    await printSummary('dummy.yaml', 'http://github.com/something', 'me/repo', true, true, DEFAULT_JOB_SUMMARY_TEMPLATE, 'success');
+    expect(setOutputMock).toBeCalledWith('step-summary-markdown', `
+You can follow the dispatched workflow dummy.yaml [here](http://github.com/something).  
+  
+✅ Workflow successful`);
   });
 });
--- src/templates/summary.ts
+++ src/templates/summary.ts
@@ -10,15 +10,14 @@ Workflow <%~ it.dispatchedWorkflow.name %> has been triggered.
 > [!NOTE]
 > The dispatched workflow is from a different repository [<%~ it.dispatchedWorkflow.repo.name %>](<%~ it.dispatchedWorkflow.repo.url %>).
 <% } %>
-
-<% if (it.waitForCompletion) { %>  
-<% if (it.dispatchedWorkflow.conclusion == 'success') { %>
+<%- if (it.waitForCompletion) { -%>
+<%- if (it.dispatchedWorkflow.conclusion == 'success') { -%>
 ✅ Workflow successful
-<% } else if (it.dispatchedWorkflow.conclusion == 'failure') { %>
+<%- } else if (it.dispatchedWorkflow.conclusion == 'failure') { -%>
 ❌ Workflow failed
-<% } else if (it.dispatchedWorkflow.conclusion == 'cancelled') { %>
+<%- } else if (it.dispatchedWorkflow.conclusion == 'cancelled') { -%>
 ✖️ Workflow cancelled
-<% } else { %>
+<%- } else { -%>
 ⁉️️ Unknown
 <% } %>
 <% } %>
