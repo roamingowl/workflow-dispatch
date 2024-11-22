b873947a944eadd885547080f420926c153e94ff
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-11-22 12:02:42 +0100

    chore(action): use ms for parsing miliseconds

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,4 +1,5 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
+import * as core from '@actions/core';
 import { parseWorkflowInputs } from '../utils';
 
 describe('parseWorkflowInputs', () => {
@@ -7,25 +8,27 @@ describe('parseWorkflowInputs', () => {
       key1: 'value1',
       key2: 2,
       key3: false,
-      key4: null,
+      key4: null
     });
   });
 
   it('should parse YAML string', () => {
-    expect(parseWorkflowInputs(`key1: "value1"
+    expect(
+      parseWorkflowInputs(`key1: "value1"
 key2: 2
 key3: false
 key4: null
 key5: |-
   line1
   line2
-`)).toEqual({
+`)
+    ).toEqual({
       key1: 'value1',
       key2: 2,
       key3: false,
       key4: null,
       key5: `line1
-line2`,
+line2`
     });
   });
 
@@ -34,9 +37,11 @@ line2`,
   });
 
   it('should fail if input is not a valid JSON or YAML', () => {
+    vi.spyOn(core, 'setFailed').mockImplementation(vi.fn());
+
     expect(() => parseWorkflowInputs('{"ssasa":invalid{{{{"s":}{')).toThrowError();
     expect(() => parseWorkflowInputs('hello')).toThrowError();
     expect(() => parseWorkflowInputs('1234')).toThrowError();
     expect(() => parseWorkflowInputs(`>key1: value1:vaule2`)).toThrowError();
   });
-})
+});
