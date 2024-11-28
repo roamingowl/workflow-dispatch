a6e593b109c5d1548b205ff6a19ade4e16a35677
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-11-28 09:24:30 +0100

    chore(action): wip

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,6 +5,8 @@ import * as main from '../main';
 const runMock = vi.spyOn(main, 'run').mockImplementation(vi.fn());
 
 vi.spyOn(core, 'setFailed').mockImplementation(vi.fn());
+vi.spyOn(core, 'error').mockImplementation(vi.fn());
+
 
 describe('index', () => {
   it('calls run when imported', async () => {
